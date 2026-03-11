36217ecfac20bbda8f93c9d3f37675c2f522b22f
Adway Rakshit <rakshitadway@gmail.com>
2026-03-11 21:32:34 +0530

    Time: 14 ms (31.92%), Space: 46.4 MB (31.45%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int r=s.length()-1;
+        int l=0;
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+        
+    }
+}
